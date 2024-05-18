4faeab784330dc3b93d3b6cd3a98da47f91e93d1
markdanielcpe <158252350+markdanielcpe@users.noreply.github.com>
2024-05-18 15:02:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ import numpy as np
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-    model = tf.keras.models.load_model('finalproj.h5')
-    return model
+    try:
+        model = tf.keras.models.load_model('finalproj.h5')
+        return model
+    except Exception as e:
+        st.write(f"Error loading the model: {e}")
+        return None
 
 def import_and_predict(image_data, model):
     size = (256, 256)
@@ -18,23 +22,23 @@ def import_and_predict(image_data, model):
     prediction = model.predict(img_reshape)
     return prediction
     
-try:
-    model = load_model()
-except Exception as e:
-    st.write(f"Error loading the model: {e}")
-    return
+model = load_model()
 
-st.write("""
-Detecting AI Generated Art vs Real Art 
-""")
-
-file = st.file_uploader("Choose any image from computer", type=["jpg", "png"])
-
-if file is None:
-    st.text("Please upload an image file")
+if model is None:
+    st.text("Error loading the model. Please try again later.")
 else:
-    image = Image.open(file)
-    st.image(image, use_column_width=True)
-    prediction = import_and_predict(image, model)
-    class_names = ['AI Generated', 'Not AI Generated']
-    string = "This art is most likely: " + class_names[np.argmax(prediction)]
+    st.write("""
+    Detecting AI Generated Art vs Real Art 
+    """)
+
+    file = st.file_uploader("Choose any image from computer", type=["jpg", "png"])
+
+    if file is None:
+        st.text("Please upload an image file")
+    else:
+        image = Image.open(file)
+        st.image(image, use_column_width=True)
+        prediction = import_and_predict(image, model)
+        class_names = ['AI Generated', 'Not AI Generated']
+        string = "This art is most likely: " + class_names[np.argmax(prediction)]
+        st.write(string)
